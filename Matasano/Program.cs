cfc028d04c0b49e48d0695281506647a14607e34
Jan Wikholm <jw@jw.fi>
2015-02-16 22:20:23 +0300

    starting set 1

--- Matasano/Program.cs
+++ Matasano/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Console;
 
 namespace Matasano
 {
@@ -10,8 +11,35 @@ namespace Matasano
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Matasano");
-            Console.ReadKey();
+            WriteLine("Hello Matasano");
+
+            //Set1.Challenge1();
+            WriteLine("Hex->Bytes f = {0}; ff = {1}", Cryptopal.HexToBytes("f")?.Length, Cryptopal.HexToBytes("ff")?.Length);
+            var str = from ch in "abc"
+                      select ch.ToString() + "foo";
+            WriteLine(String.Join(" ", str));
+            ReadKey();
+        }
+
+
+    }
+
+    class Set1
+    {
+        public static void Challenge1()
+        {
+            var b64_input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+            var b64_output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+            try
+            {
+                var b64_bytes = Cryptopal.HexToBytes(b64_input);
+                WriteLine("{0} = base64({1})\n{2} = expected", Cryptopal.Base64Encode(b64_bytes), b64_input, b64_output);
+            } catch (ArgumentNullException e)
+            {
+                WriteLine("Failed! {0}", e);
+            }
+            
+
         }
     }
 }
